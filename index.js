31bd8ad710c9a6880b6a789f14926e26fb6af788
Азизбеков Руми Саъдиевич <r.azizbekov@mkit.local>
2025-03-27 13:28:37 +0300

    POST endpoint

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get('/fio',(req,res)=>{
     const familiya = 'Азизбеков'
     res.send(`Привет, ${imya} ${familiya}!`)
 })
-
+app.post('/add',async(req,res)=>{
+    const {login,password} = req.body
+    await User.create({login,password})
+    res.send(`user was created`)
+})
+async function start(){
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
         app.listen(process.env.PORT || 8080,()=>{
-            console.log(`server is running on port: ${process.env.PORT || 8080}`)})
+            console.log(`server is running on port: ${process.env.PORT || 8080}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
